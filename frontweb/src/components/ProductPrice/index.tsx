1f52dd1b87c08493ff91798dafd954bae08ffe1d
DiegoVP66 <dvpdev33@gmail.com>
2022-01-17 16:37:49 -0300

    Props ProductCard, ProdutPrice

--- frontweb/src/components/ProductPrice/index.tsx
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -1,13 +1,16 @@
 import './styles.css';
 
-const ProdutPrice = () => {
+type Props = {
+  price: number;
+};
 
-    return (
-       <div className="product-price-container">
-           <span>R$</span>
-           <h3>2345.67</h3>
-       </div> 
-    );
-}
+const ProdutPrice = ({ price }: Props) => {
+  return (
+    <div className="product-price-container">
+      <span>R$</span>
+      <h3>{price}</h3>
+    </div>
+  );
+};
 
 export default ProdutPrice;
