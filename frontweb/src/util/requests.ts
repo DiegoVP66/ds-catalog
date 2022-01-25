05b9c0080685042b108ad9cfd6be7fd3dbd4b420
DiegoVP66 <dvpdev33@gmail.com>
2022-01-25 19:38:35 -0300

    HasAnyRoles

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -75,7 +75,7 @@ export const getAuthData = () => {
 
 export const removeAuthData = () => {
   localStorage.removeItem(tokenKey);
-}
+};
 
 axios.interceptors.request.use(
   function (config) {
@@ -88,7 +88,6 @@ axios.interceptors.request.use(
 
 axios.interceptors.response.use(
   function (response) {
-    console.log('INTERCEPTOR RESPOSTA COM SUCESSO');
     return response;
   },
   function (error) {
@@ -111,4 +110,26 @@ export const isAuthenticated = (): boolean => {
   const tokenData = getTokenData();
 
   return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
+};
+
+export const hasAnyRoles = (roles: Role[]): boolean => {
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+  if (tokenData !== undefined) {
+    return roles.some((role) => tokenData.authorities.includes(role));
+  }
+
+  /*
+  if(tokenData !== undefined) {
+    for(var i = 0; i < roles.length; i++){
+      if(tokenData.authorities.includes(roles[i])){
+        return true;
+      }
+    }
+  }*/
+
+  return false;
 };
