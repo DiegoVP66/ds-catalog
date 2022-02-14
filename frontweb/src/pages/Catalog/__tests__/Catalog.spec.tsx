32b833e98986e664c351b4e962e4e583c00000b8
DiegoVP66 <dvpdev33@gmail.com>
2022-02-13 23:08:45 -0300

    MSW mock server, fixtures

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,6 +2,17 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import history from 'util/history';
 import Catalog from '..';
+import { server } from './fixtures';
+
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  server.resetHandlers();
+});
+afterAll(() => {
+  server.close();
+});
 
 test('should render Catalog with products', async () => {
   render(
@@ -13,6 +24,6 @@ test('should render Catalog with products', async () => {
   expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
 
   await waitFor(() => {
-    expect(screen.getByText('Smart TV')).toBeInTheDocument();
+    expect(screen.getByText('Macbook Pro')).toBeInTheDocument();
   });
 });
