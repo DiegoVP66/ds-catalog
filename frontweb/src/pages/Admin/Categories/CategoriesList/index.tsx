8c9187b08c7f56c90cac983934ff5370c7353f8d
DiegoVP66 <dvpdev33@gmail.com>
2022-02-22 14:50:37 -0300

    New category button add

--- frontweb/src/pages/Admin/Categories/CategoriesList/index.tsx
+++ frontweb/src/pages/Admin/Categories/CategoriesList/index.tsx
@@ -1,9 +1,11 @@
 import { AxiosRequestConfig } from 'axios';
 import { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Category } from 'types/category';
 import { SpringPage } from 'types/vendor/spring';
 import { requestBackend } from 'util/requests';
 import CategoryCrudCard from '../CategoryCrudCard';
+import './styles.css';
 
 const CategoriesList = () => {
   const [page, setPage] = useState<SpringPage<Category>>();
@@ -29,6 +31,12 @@ const CategoriesList = () => {
 
   return (
     <div>
+      <Link to="/admin/categories/create">
+        <button className="btn btn-primary text-white btn-crud-add">
+          ADICIONAR{' '}
+          <span className="new-category-text-button">NOVA CATEGORIA</span>
+        </button>
+      </Link>
       {page?.content.map((category) => (
         <div key={category.id}>
           <CategoryCrudCard category={category} onDelete={getCategories} />
